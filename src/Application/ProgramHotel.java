5d0a8faf17de5e4bbdfe91f4934ff421fecb9ceb
tales112 <tales.trab@gmail.com>
2020-11-19 16:52:20 -0300

    Bad solution

--- src/Application/ProgramHotel.java
+++ src/Application/ProgramHotel.java
@@ -34,17 +34,13 @@ public class ProgramHotel {
 			System.out.println("Check-out Date");
 			checkOut = cs.parse(sc.next());
 
-			Date now = new Date();
-			if (checkIn.before(now) || checkOut.before(now)) {
-				System.out.println("Error in reservation: Reservation dates for update must be future dates");
-			} else if (!checkOut.after(checkIn)) {
-				System.out.println("Error in reseration: Check-out date must be after check-in Date");
-
+			String error = reservation.updateDates(checkIn, checkOut);
+			if (error != null) {
+				System.out.println("Eror in reservation" + error);
 			} else {
-
-				reservation.updateDates(checkIn, checkOut);
 				System.out.println("Reservation" + reservation);
 			}
+
 		}
 
 		sc.close();
--- src/Entities/Reservation.java
+++ src/Entities/Reservation.java
@@ -9,6 +9,7 @@ public class Reservation {
 	private Date checkin;
 	private Date checkout;
 	SimpleDateFormat cs = new SimpleDateFormat("dd/MM/yyyy");
+
 	public Reservation(Integer roomNumber, Date checkin, Date checkout) {
 		this.roomNumber = roomNumber;
 		this.checkin = checkin;
@@ -35,16 +36,26 @@ public class Reservation {
 	public Integer duration() {
 		return checkout.getDate() - checkin.getDate();
 	}
-	
-	public void updateDates(Date checkin, Date checkout) {
-		this.checkin = checkin;
-		this.checkout = checkout;
+
+	public String updateDates(Date checkIn, Date checkOut) {
+		Date now = new Date();
+		if (checkIn.before(now) || checkOut.before(now)) {
+			return ("Error in reservation: Reservation dates for update must be future dates");
+		}
+		if (!checkOut.after(checkIn)) {
+			return ("Error in reseration: Check-out date must be after check-in Date");
+
+		}
+
+		this.checkin = checkIn;
+		this.checkout = checkOut;
+		return null;
 	}
 
 	@Override
 	public String toString() {
-		return "Reservation : roomNumber:" + roomNumber + ", checkin:" + cs.format(checkin) + ", checkout:" + cs.format(checkout)
-				+ ", duration()=" + duration() + "Nights";
+		return "Reservation : roomNumber:" + roomNumber + ", checkin:" + cs.format(checkin) + ", checkout:"
+				+ cs.format(checkout) + ", duration()=" + duration() + "Nights";
 	}
-	
+
 }
